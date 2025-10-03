70c58478bcbea9658f3823dcc879c0965f03aa54
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2025-10-03 12:49:53 -0400

    Problem 8 Sanaa

--- index.js
+++ index.js
@@ -65,10 +65,10 @@ function add(num1, num2) {
         console.log(sum)
     }
 }
-add(2, 4)     //returns 6
-add(10, -5)   //returns 5add(3, 7.5)   //returns 10.5
-add(5, 5)     //returns 30 because the sum of 5 + 5 = 10 and 30 is triple that
-add(6, 6)     //returns 36 because the sum of 6 + 6 = 12 and 36 is triple that
+//add(2, 4)     //returns 6
+//add(10, -5)   //returns 5add(3, 7.5)   //returns 10.5
+//add(5, 5)     //returns 30 because the sum of 5 + 5 = 10 and 30 is triple that
+//add(6, 6)     //returns 36 because the sum of 6 + 6 = 12 and 36 is triple that
 
 //Probelem 7 Syed
 
@@ -82,3 +82,12 @@ function isNegative (n) {
 //console.log(isNegative(3))
 //console.log(isNegative(-2))
 //console.log(isNegative(Math.Pi))
+
+// Problem 8 Problem
+
+function triangleArea(height, base) {
+    let area = base / 2 * height
+    console.log(area)
+}
+// triangleArea(5, 7)    //returns 17.5
+// triangleArea(6, 8)    //returns 24
